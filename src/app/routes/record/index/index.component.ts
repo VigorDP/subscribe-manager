61ea5071fb676f24b2fdedb99512d86cd2faa836
fyc <15926339107@163.com>
2020-04-27 19:13:18 +0800

    feat: 接口对接

--- src/app/routes/record/index/index.component.ts
+++ src/app/routes/record/index/index.component.ts
@@ -4,7 +4,7 @@ import { _HttpClient } from '@delon/theme';
 import { STComponent, STChange, STColumn } from '@delon/abc';
 import { RestService } from '@app/service';
 import { query, defaultQuery, pages, total, loading, data, selectedRows, selectedRow } from '@app/common';
-
+import dayjs from 'dayjs/esm';
 @Component({
   templateUrl: './index.component.html',
   styleUrls: ['../../../common/styles/common.css', './index.component.scss'],
@@ -19,23 +19,24 @@ export class ProjectComponent implements OnInit {
   selectedRows = selectedRows;
   selectedRow = selectedRow;
   columns: STColumn[] = [
-    { title: '预约人手机号', index: 'socialName' },
-    { title: '入园人姓名', index: 'contact' },
-    { title: '入园人身份证号', index: 'contactTel' },
-    { title: '状态', index: 'address' },
-    { title: '预约日期', index: 'area' },
-    { title: '核销码', index: 'descr' },
-    { title: '核销时间', index: 'gmtCreate' },
+    { title: '预约人手机号', index: 'tel' },
+    { title: '入园人姓名', index: 'name' },
+    { title: '入园人身份证号', index: 'idCard' },
+    { title: '状态', index: 'status' },
+    { title: '预约日期', index: 'appointDate' },
+    { title: '核销码', index: 'code' },
+    { title: '核销时间', index: 'checkoutTime' },
   ];
+
+  stat = {};
 
   @ViewChild('st', { static: true })
   st: STComponent;
 
   StatusList = [
-    { label: '全部', value: 'all' },
-    { label: '未核销', value: 'ready' },
-    { label: '已核销', value: 'finish' },
-    { label: '已过期', value: 'expire' },
+    { label: '可用', value: 1 },
+    { label: '已核销', value: 2 },
+    { label: '已过期', value: 3 },
   ];
 
   constructor(
@@ -48,22 +49,37 @@ export class ProjectComponent implements OnInit {
   ngOnInit() {
     this.query = { ...defaultQuery };
     this.getData();
+    this.getSubscribeInfo();
+  }
+
+  getSubscribeInfo() {
+    this.api.getSubscribeInfo({}).subscribe(res => {
+      if (res.code === '0') {
+        this.stat = res.data;
+      } else {
+        this.msg.error('统计信息获取失败');
+      }
+      this.cdr.detectChanges();
+    });
   }
 
   getData(pageIndex?: number) {
     this.loading = true;
     this.query.pageNo = pageIndex ? pageIndex : this.query.pageNo;
-    // this.api.getSubscribeList(this.query).subscribe(res => {
-    //   this.loading = false;
-    //   const { rows, total: totalItem } = res.data || { rows: [], total: 0 };
-    //   this.data = rows;
-    //   this.total = totalItem;
-    //   this.pages = {
-    //     ...this.pages,
-    //     total: `共 ${totalItem} 条记录`,
-    //   };
-    //   this.cdr.detectChanges();
-    // });
+    if (this.query.startAppoint) {
+      this.query.startAppoint = dayjs(this.query.startAppoint).format('YYYY-MM-DD');
+    }
+    this.api.getSubscribeList(this.query).subscribe(res => {
+      this.loading = false;
+      const { rows, total: totalItem } = res.data || { rows: [], total: 0 };
+      this.data = rows;
+      this.total = totalItem;
+      this.pages = {
+        ...this.pages,
+        total: `共 ${totalItem} 条记录`,
+      };
+      this.cdr.detectChanges();
+    });
   }
 
   stChange(e: STChange) {
--- src/app/service/rest.service.ts
+++ src/app/service/rest.service.ts
@@ -8,7 +8,10 @@ export class RestService {
   constructor(private http: _HttpClient, private settings: SettingsService) {}
 
   // 预约管理-获取列表
-  getSubscribeList = (params: any) => this.http.post(`${PREFIX}social/list`, params);
+  getSubscribeList = (params: any) => this.http.post(`${PREFIX}scenery/appoint/list`, params);
+
+  // 预约管理-统计信息
+  getSubscribeInfo = (params: any) => this.http.get(`${PREFIX}scenery/appoint/stat`, params);
 }
 
 function paramsWithExtraParams(params, community) {
